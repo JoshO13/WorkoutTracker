1c78b183418b975200f630722deacbc47d4f3099
JoshO13 <joshhockey19@gmail.com>
2024-08-06 18:49:09 -0500

    Protecting react routes and verifying authenticated requests

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,24 +1,32 @@
 import { useEffect } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutsForm from '../components/WorkoutsForm'
 
 const Home = () => {
     const {workouts, dispatch} = useWorkoutsContext()
-
+    const {user} = useAuthContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                } 
+            })
             const json = await response.json()
 
             if (response.ok) {
                 dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
-        fetchWorkouts()
-    }, [dispatch])
+        if (user) {
+            fetchWorkouts()
+        }
+        
+    }, [dispatch, user])
 
     return (
         <div className="home">
